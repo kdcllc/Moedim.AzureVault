b4abcf2668b184ddf023ef3716c2333b930229d9
kdcllc <info@kingdavidconsulting.com>
2022-11-08 15:50:25 -0500

    #2 upgrading to .net 7.0 release

--- test/Moedim.AzureVault.UnitTest/ConfigurationBuilderTests.cs
+++ test/Moedim.AzureVault.UnitTest/ConfigurationBuilderTests.cs
@@ -1,61 +1,61 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+
 using Moedim.AzureVault.Options;
 
-namespace Moedim.AzureVault.UnitTest
+namespace Moedim.AzureVault.UnitTest;
+
+public class ConfigurationBuilderTests
 {
-    public class ConfigurationBuilderTests
+    [Fact]
+    public void Validating_Options_ThrowsOptions_ValidationException()
     {
-        [Fact]
-        public void Validating_Options_ThrowsOptions_ValidationException()
+        var builder = new ConfigurationBuilder();
+
+        var dict = new Dictionary<string, string>
         {
-            var builder = new ConfigurationBuilder();
+            { "AzureVault:BaseUrl", string.Empty }
+        };
 
-            var dict = new Dictionary<string, string>
-            {
-                { "AzureVault:BaseUrl", string.Empty }
-            };
+        builder.AddInMemoryCollection(dict!);
 
-            builder.AddInMemoryCollection(dict);
+        var services = new ServiceCollection();
 
-            var services = new ServiceCollection();
+        services.AddSingleton<IConfiguration>(builder.Build());
 
-            services.AddSingleton<IConfiguration>(builder.Build());
+        services.AddOptions<AzureVaultOptions>().ValidateDataAnnotations();
 
-            services.AddOptions<AzureVaultOptions>().ValidateDataAnnotations();
+        var sp = services.BuildServiceProvider();
 
-            var sp = services.BuildServiceProvider();
+        var options = sp.GetRequiredService<IOptions<AzureVaultOptions>>();
 
-            var options = sp.GetRequiredService<IOptions<AzureVaultOptions>>();
+        Assert.Throws<OptionsValidationException>(() => options.Value);
+    }
 
-            Assert.Throws<OptionsValidationException>(() => options.Value);
-        }
+    [Theory]
+    [InlineData("Development")]
+    [InlineData("Staging")]
+    [InlineData("Production")]
+    public void Create_AzureVault_Client_Per_Enviroment_Successfully(string env)
+    {
+        var builder = new ConfigurationBuilder();
 
-        [Theory]
-        [InlineData("Development")]
-        [InlineData("Staging")]
-        [InlineData("Production")]
-        public void Create_AzureVault_Client_Per_Enviroment_Successfully(string env)
+        var dict = new Dictionary<string, string>
         {
-            var builder = new ConfigurationBuilder();
+            { "AzureVault:BaseUrl", "https://bet.vault.azure.net/;https://kdcllc.vault.azure.net/" }
+        };
 
-            var dict = new Dictionary<string, string>
-            {
-                { "AzureVault:BaseUrl", "https://bet.vault.azure.net/;https://kdcllc.vault.azure.net/" }
-            };
+        builder.AddInMemoryCollection(dict!);
 
-            builder.AddInMemoryCollection(dict);
+        builder.AddAzureKeyVault(env, tokenAuthRetry: 1);
 
-            builder.AddAzureKeyVault(env, tokenAuthRetry: 1);
+        var list = builder.Sources.ToList();
+        var found = list.Where(x => x.GetType()?.FullName?.Contains("AzureKeyVaultConfigurationSource") ?? false);
 
-            var list = builder.Sources.ToList();
-            var found = list.Where(x => x.GetType().FullName.Contains("AzureKeyVaultConfigurationSource"));
+        Assert.Equal(4, found.Count());
 
-            Assert.Equal(4, found.Count());
-
-            // var config = builder.Build();
-            // Assert.NotNull(config);
-        }
+        // var config = builder.Build();
+        // Assert.NotNull(config);
     }
 }
